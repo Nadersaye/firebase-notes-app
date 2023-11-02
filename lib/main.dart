1d906b22c1d34dc5b3e6b41673ac14b9676a09e1
Nader <ns1568@fayoum.edu.eg>
2023-11-03 00:48:36 +0200

    change home page name

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_training/features/auth/presentation/views/login_view.dart';
-import 'package:firebase_training/features/home/presentation/views/home_page.dart';
+import 'package:firebase_training/features/home/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'features/auth/presentation/views/register_view.dart';
