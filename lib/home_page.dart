05738357d66698c15561035253f53930e2965ce6
Nader <ns1568@fayoum.edu.eg>
2023-11-02 17:20:26 +0200

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Firebase install'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+                Navigator.of(context)
+                    .pushNamedAndRemoveUntil("login", (route) => false);
+              },
+              icon: const Icon(Icons.exit_to_app))
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'dart:io';
+import 'package:email_otp/email_otp.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_training/features/auth/presentation/views/login.dart';
+import 'package:firebase_training/features/auth/presentation/views/otp_email.dart';
+import 'package:firebase_training/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'features/auth/presentation/views/signup.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+              apiKey: "AIzaSyBd8pXo0SZ0SO-ox_wNz471E7WAujq0p9w",
+              appId: "1:335388615801:android:837c8215672db42ad620a8",
+              messagingSenderId: "335388615801",
+              projectId: "fir-training-828f4"))
+      : await Firebase.initializeApp();
+  runApp(const FirebaseTraining());
+}
+
+class FirebaseTraining extends StatefulWidget {
+  const FirebaseTraining({super.key});
+
+  @override
+  State<FirebaseTraining> createState() => _FirebaseTrainingState();
+}
+
+class _FirebaseTrainingState extends State<FirebaseTraining> {
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        debugPrint('User is currently signed out!');
+      } else {
+        debugPrint('User is signed in!');
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FirebaseAuth.instance.currentUser == null
+          ? const LoginView()
+          : const HomePage(),
+      routes: {
+        "signup": (context) => const SignUp(),
+        "login": (context) => const LoginView(),
+        "homepage": (context) => const HomePage(),
+        "otpscreen": (context) => OtpScreen(
+              myauth: ModalRoute.of(context)!.settings.arguments as EmailOTP,
+            )
+      },
+    );
+  }
+}
