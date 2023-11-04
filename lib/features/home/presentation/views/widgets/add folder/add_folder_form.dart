220b38b23473afcdfa4ec282c13fbe47c4f24a5b
Nader <ns1568@fayoum.edu.eg>
2023-11-05 00:05:06 +0200

    trigger add category cubit

--- lib/features/home/presentation/views/widgets/add folder/add_folder_form.dart
+++ lib/features/home/presentation/views/widgets/add folder/add_folder_form.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_training/core/widgets/custom_button.dart';
+import 'package:firebase_training/features/auth/presentation/views/widgets/custom_awesome_dialog.dart';
+import 'package:firebase_training/features/home/presentation/manager/add%20category%20cubit/add_category_cubit.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../../../../core/widgets/custom_textformfield.dart';
 
 class AddFolderForm extends StatefulWidget {
@@ -16,38 +18,56 @@ class _AddFolderFormState extends State<AddFolderForm> {
   AutovalidateMode? autovalidateMode = AutovalidateMode.disabled;
   TextEditingController folder = TextEditingController();
   GlobalKey<FormState> formState = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
-    return Form(
-      autovalidateMode: autovalidateMode,
-      key: formState,
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            CustomTextForm(
-                hinttext: 'enter folder name',
-                mycontroller: folder,
-                textFormFieldvalidator: (String? value) {
-                  if (value?.isEmpty ?? true) {
-                    return "can't be empty";
+    return BlocListener<AddCategoryCubit, AddCategoryState>(
+      listener: (context, state) {
+        if (state is AddCategorySuccess) {
+          debugPrint(state.successMessage);
+          Navigator.of(context).pushReplacementNamed("homepage");
+        } else if (state is AddCategoryFailure) {
+          customAwesomeDialog(
+              context: context,
+              titleText: 'Error',
+              contentText: state.toString(),
+              color: Colors.red);
+        }
+      },
+      child: Form(
+        autovalidateMode: autovalidateMode,
+        key: formState,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              CustomTextForm(
+                  hinttext: 'enter folder name',
+                  mycontroller: folder,
+                  textFormFieldvalidator: (String? value) {
+                    if (value?.isEmpty ?? true) {
+                      return "can't be empty";
+                    } else {
+                      return null;
+                    }
+                  }),
+              const SizedBox(
+                height: 20,
+              ),
+              CustomButton(
+                title: 'Add folder',
+                onPressed: () {
+                  if (formState.currentState!.validate()) {
+                    BlocProvider.of<AddCategoryCubit>(context)
+                        .addCategory(folder);
                   } else {
-                    return null;
+                    autovalidateMode = AutovalidateMode.always;
+                    setState(() {});
                   }
-                }),
-            const SizedBox(
-              height: 20,
-            ),
-            CustomButton(
-              title: 'Add folder',
-              onPressed: () {
-                if (formState.currentState!.validate()) {
-                } else {
-                  autovalidateMode = AutovalidateMode.always;
-                }
-              },
-            ),
-          ],
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
