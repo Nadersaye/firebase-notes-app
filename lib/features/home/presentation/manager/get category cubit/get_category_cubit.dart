c5ea8a31ef140329a5a492dbaf00a1f90f662a09
Nader <ns1568@fayoum.edu.eg>
2023-11-05 23:10:33 +0200

    modify add category cubit to link with user's id

--- lib/features/home/presentation/manager/get category cubit/get_category_cubit.dart
+++ lib/features/home/presentation/manager/get category cubit/get_category_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_training/core/constants.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
@@ -7,14 +8,15 @@ part 'get_category_state.dart';
 
 class GetCategoryCubit extends Cubit<GetCategoryState> {
   GetCategoryCubit() : super(GetCategoryInitial());
-
-  List<QueryDocumentSnapshot> categories = [];
-
+  late List<QueryDocumentSnapshot> categories;
   getCategory() async {
     emit(GetCategoryLoading());
+    categories = [];
     try {
-      QuerySnapshot response =
-          await FirebaseFirestore.instance.collection(firstCollection).get();
+      QuerySnapshot response = await FirebaseFirestore.instance
+          .collection(firstCollection)
+          .where("id", isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+          .get();
       categories.addAll(response.docs);
       emit(GetCategorySuccess());
     } catch (e) {
