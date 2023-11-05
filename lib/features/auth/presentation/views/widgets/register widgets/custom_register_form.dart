4bb861613b329c9423ac746314ff689e9b4f236a
Nader <ns1568@fayoum.edu.eg>
2023-11-05 23:02:45 +0200

    wrap custom register form with blocConsumer

--- lib/features/auth/presentation/views/widgets/register widgets/custom_register_form.dart
+++ lib/features/auth/presentation/views/widgets/register widgets/custom_register_form.dart
@@ -1,8 +1,9 @@
-import 'package:awesome_dialog/awesome_dialog.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_training/core/widgets/custom_awesome_dialog.dart';
+import 'package:firebase_training/core/widgets/custom_toast.dart';
 import 'package:flutter/material.dart';
-import '../custom_awesome_dialog.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../../../../core/widgets/custom_button.dart';
+import '../../../manager/register cubit/register_cubit.dart';
 import '../custom_logo_auth.dart';
 import '../../../../../../core/widgets/custom_textformfield.dart';
 
@@ -24,120 +25,108 @@ class _CustomRegisterFormState extends State<CustomRegisterForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      autovalidateMode: autovalidateMode,
-      key: formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(height: 50),
-          const CustomLogoAuth(),
-          Container(height: 20),
-          const Text("SignUp",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-          Container(height: 10),
-          const Text("SignUp To Continue Using The App",
-              style: TextStyle(color: Colors.grey)),
-          Container(height: 20),
-          const Text(
-            "username",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-          ),
-          Container(height: 10),
-          CustomTextForm(
-            hinttext: "ُEnter Your username",
-            mycontroller: username,
-            textFormFieldvalidator: (String? input) {
-              if (input?.isEmpty ?? true) {
-                return 'enter the username ';
-              } else {
-                return null;
-              }
-            },
-          ),
-          Container(height: 20),
-          const Text(
-            "Email",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-          ),
-          Container(height: 10),
-          CustomTextForm(
-            hinttext: "ُEnter Your Email",
-            mycontroller: email,
-            textFormFieldvalidator: (String? input) {
-              if (input?.isEmpty ?? true) {
-                return 'enter the email ';
-              } else {
-                return null;
-              }
-            },
-          ),
-          Container(height: 10),
-          const Text(
-            "Password",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-          ),
-          Container(height: 10),
-          CustomTextForm(
-            hinttext: "ُEnter Your Password",
-            mycontroller: password,
-            textFormFieldvalidator: (String? input) {
-              if (input?.isEmpty ?? true) {
-                return 'enter the password ';
-              } else {
-                return null;
-              }
-            },
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 10, bottom: 20),
-            alignment: Alignment.topRight,
-            child: const Text(
-              "Forgot Password ?",
-              style: TextStyle(
-                fontSize: 14,
+    return BlocConsumer<RegisterCubit, RegisterState>(
+      listener: (context, state) {
+        if (state is RegisterFailure) {
+          customAwesomeDialog(
+              context: context,
+              titleText: 'Error',
+              contentText: state.errorMessage);
+        } else if (state is RegisterSuccess) {
+          customToast(message: state.successMessage);
+        }
+      },
+      builder: (context, state) {
+        return Form(
+          autovalidateMode: autovalidateMode,
+          key: formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(height: 50),
+              const CustomLogoAuth(),
+              Container(height: 20),
+              const Text("SignUp",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+              Container(height: 10),
+              const Text("SignUp To Continue Using The App",
+                  style: TextStyle(color: Colors.grey)),
+              Container(height: 20),
+              const Text(
+                "username",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
               ),
-            ),
+              Container(height: 10),
+              CustomTextForm(
+                hinttext: "ُEnter Your username",
+                mycontroller: username,
+                textFormFieldvalidator: (String? input) {
+                  if (input?.isEmpty ?? true) {
+                    return 'enter the username ';
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              Container(height: 20),
+              const Text(
+                "Email",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              Container(height: 10),
+              CustomTextForm(
+                hinttext: "ُEnter Your Email",
+                mycontroller: email,
+                textFormFieldvalidator: (String? input) {
+                  if (input?.isEmpty ?? true) {
+                    return 'enter the email ';
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              Container(height: 10),
+              const Text(
+                "Password",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              Container(height: 10),
+              CustomTextForm(
+                hinttext: "ُEnter Your Password",
+                mycontroller: password,
+                textFormFieldvalidator: (String? input) {
+                  if (input?.isEmpty ?? true) {
+                    return 'enter the password ';
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 10, bottom: 20),
+                alignment: Alignment.topRight,
+                child: const Text(
+                  "Forgot Password ?",
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+              CustomButton(
+                  title: "SignUp",
+                  onPressed: () async {
+                    if (formKey.currentState!.validate()) {
+                      BlocProvider.of<RegisterCubit>(context)
+                          .validateRegister(email, password);
+                    } else {
+                      autovalidateMode = AutovalidateMode.always;
+                      setState(() {});
+                    }
+                  }),
+            ],
           ),
-          CustomButton(
-              title: "SignUp",
-              onPressed: () async {
-                if (formKey.currentState!.validate()) {
-                  await validateRegister(context);
-                } else {
-                  autovalidateMode = AutovalidateMode.always;
-                }
-              }),
-        ],
-      ),
+        );
+      },
     );
-  }
-
-  Future<void> validateRegister(BuildContext context) async {
-    try {
-      final credential =
-          await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: email.text,
-        password: password.text,
-      );
-      credential.user!.sendEmailVerification();
-      customAwesomeDialog(
-          context: context,
-          titleText: 'Warning',
-          contentText: "check your email's inbox to verify your account ",
-          color: Colors.amber,
-          dialogType: DialogType.warning);
-      Future.delayed(const Duration(seconds: 3), () {
-        Navigator.of(context).pushReplacementNamed('login');
-      });
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'weak-password') {
-        debugPrint('The password provided is too weak.');
-      } else if (e.code == 'email-already-in-use') {
-        debugPrint('The account already exists for that email.');
-      }
-    } catch (e) {
-      print(e);
-    }
   }
 }
