31b96a8aa75794e5597cf60058e3edc68752894d
Nader <ns1568@fayoum.edu.eg>
2023-11-05 23:08:13 +0200

    modify gridview item to deal with onlongPress

--- lib/features/home/presentation/views/widgets/home widgets/gridview_folders_item.dart
+++ lib/features/home/presentation/views/widgets/home widgets/gridview_folders_item.dart
@@ -2,20 +2,24 @@ import 'package:firebase_training/core/constants.dart';
 import 'package:flutter/material.dart';
 
 class GridViewFolderItem extends StatelessWidget {
-  const GridViewFolderItem({super.key, required this.title});
+  const GridViewFolderItem({super.key, required this.title, this.onLongPress});
   final String title;
+  final void Function()? onLongPress;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 3,
-      child: Column(
-        children: [
-          Image.asset(
-            folderLogo,
-            height: 100,
-          ),
-          Text(title)
-        ],
+    return InkWell(
+      onLongPress: onLongPress,
+      child: Card(
+        elevation: 3,
+        child: Column(
+          children: [
+            Image.asset(
+              folderLogo,
+              height: 100,
+            ),
+            Text(title)
+          ],
+        ),
       ),
     );
   }
