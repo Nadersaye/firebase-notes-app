f3f41c481cd8675ed224c1c375b9b0cef122e943
Nader <ns1568@fayoum.edu.eg>
2023-11-06 23:12:34 +0200

    modify custom text form(max lines & border )

--- lib/core/widgets/custom_textformfield.dart
+++ lib/core/widgets/custom_textformfield.dart
@@ -4,15 +4,18 @@ class CustomTextForm extends StatelessWidget {
   final String hinttext;
   final TextEditingController mycontroller;
   final String? Function(String?)? textFormFieldvalidator;
+  final int maxLines;
   const CustomTextForm(
       {super.key,
       required this.hinttext,
       required this.mycontroller,
-      required this.textFormFieldvalidator});
+      required this.textFormFieldvalidator,
+      this.maxLines = 1});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      maxLines: maxLines,
       validator: textFormFieldvalidator,
       controller: mycontroller,
       decoration: InputDecoration(
@@ -22,13 +25,16 @@ class CustomTextForm extends StatelessWidget {
               const EdgeInsets.symmetric(vertical: 2, horizontal: 20),
           filled: true,
           fillColor: Colors.grey[200],
-          border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(50),
+          border: const OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(25)),
               borderSide:
-                  const BorderSide(color: Color.fromARGB(255, 184, 184, 184))),
+                  BorderSide(color: Color.fromARGB(255, 184, 184, 184))),
           enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(50),
-              borderSide: const BorderSide(color: Colors.grey))),
+              borderRadius: BorderRadius.circular(25),
+              borderSide: const BorderSide(color: Colors.grey)),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25),
+              borderSide: const BorderSide(color: Colors.orange))),
     );
   }
 }
