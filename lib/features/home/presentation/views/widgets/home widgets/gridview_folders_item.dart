1de194be4ff3e33dc396c664d45875c39bc94fd8
Nader <ns1568@fayoum.edu.eg>
2023-11-06 23:14:38 +0200

    modify grid view folder item(add onTap)

--- lib/features/home/presentation/views/widgets/home widgets/gridview_folders_item.dart
+++ lib/features/home/presentation/views/widgets/home widgets/gridview_folders_item.dart
@@ -2,12 +2,15 @@ import 'package:firebase_training/core/constants.dart';
 import 'package:flutter/material.dart';
 
 class GridViewFolderItem extends StatelessWidget {
-  const GridViewFolderItem({super.key, required this.title, this.onLongPress});
+  const GridViewFolderItem(
+      {super.key, required this.title, this.onLongPress, this.onTap});
   final String title;
   final void Function()? onLongPress;
+  final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      onTap: onTap,
       onLongPress: onLongPress,
       child: Card(
         elevation: 3,
