8fa33fd65637b4f666ee8505b28f8cf072eb81e3
Nader <ns1568@fayoum.edu.eg>
2023-11-06 23:10:33 +0200

    modify custom awesome dialog

--- lib/core/widgets/custom_awesome_dialog.dart
+++ lib/core/widgets/custom_awesome_dialog.dart
@@ -1,16 +1,19 @@
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 
-void customAwesomeDialog({
-  required BuildContext context,
-  required String titleText,
-  required String contentText,
-  Color? color,
-  void Function()? btnOkOnPress,
-  void Function()? btnCancelOnPress,
-  DialogType dialogType = DialogType.error,
-}) {
+void customAwesomeDialog(
+    {required BuildContext context,
+    required String titleText,
+    required String contentText,
+    Color? color,
+    void Function()? btnOkOnPress,
+    void Function()? btnCancelOnPress,
+    DialogType dialogType = DialogType.error,
+    String cancelText = 'Cancel',
+    String okText = 'Ok'}) {
   AwesomeDialog(
+    btnCancelText: cancelText,
+    btnOkText: okText,
     context: context,
     dialogType: dialogType,
     animType: AnimType.rightSlide,
