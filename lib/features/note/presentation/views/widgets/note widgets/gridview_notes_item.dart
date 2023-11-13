ba8ab7b183424cd21aa6bf175856377a866993bb
Nader <ns1568@fayoum.edu.eg>
2023-11-14 01:50:39 +0200

    display note image

--- lib/features/note/presentation/views/widgets/note widgets/gridview_notes_item.dart
+++ lib/features/note/presentation/views/widgets/note widgets/gridview_notes_item.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_training/features/note/data/models/note_model.dart';
+import 'package:firebase_training/features/note/presentation/manager/get%20note%20cubit/get_note_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class GridViewNoteItem extends StatelessWidget {
-  const GridViewNoteItem({super.key, required this.title, this.onLongPress});
-  final String title;
+  const GridViewNoteItem({super.key, this.onLongPress, required this.note});
+  final NoteModel note;
   final void Function()? onLongPress;
   @override
   Widget build(BuildContext context) {
@@ -13,7 +16,23 @@ class GridViewNoteItem extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Column(
-            children: [Text(title)],
+            children: [
+              Text(note.note),
+              BlocBuilder<GetNoteCubit, GetNoteState>(
+                builder: (context, state) {
+                  return note.imageUrl != ""
+                      ? Image.network(
+                          note.imageUrl,
+                          height: 90,
+                          width: 150,
+                          fit: BoxFit.fill,
+                        )
+                      : const SizedBox(
+                          height: 20,
+                        );
+                },
+              ),
+            ],
           ),
         ),
       ),
