3a63489a2de963129bb8eda38a25fe81a5b892f9
Nader <ns1568@fayoum.edu.eg>
2023-11-14 01:47:44 +0200

    use note model fromSnapshot to recieve data

--- lib/features/note/presentation/manager/get note cubit/get_note_cubit.dart
+++ lib/features/note/presentation/manager/get note cubit/get_note_cubit.dart
@@ -1,24 +1,27 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_training/core/constants.dart';
+import 'package:firebase_training/features/note/data/models/note_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
-
 part 'get_note_state.dart';
 
 class GetNoteCubit extends Cubit<GetNoteState> {
   GetNoteCubit() : super(GetNoteInitial());
-  late List<QueryDocumentSnapshot> notes;
+  late List<NoteModel> notes;
   late String mainPath;
   getNote() async {
     emit(GetNoteLoading());
     notes = [];
     try {
-      QuerySnapshot response = await FirebaseFirestore.instance
+      var response = await FirebaseFirestore.instance
           .collection(firstCollection)
           .doc(mainPath)
           .collection(noteCollection)
           .get();
-      notes.addAll(response.docs);
+      response.docs.map((e) => notes.add(NoteModel.fromSnapShot(e)));
+      notes = response.docs
+          .map((element) => NoteModel.fromSnapShot(element))
+          .toList();
       emit(GetNoteSuccess());
     } catch (e) {
       emit(GetNoteFailure(error: e.toString()));
