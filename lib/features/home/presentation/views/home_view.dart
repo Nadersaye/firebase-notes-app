701cb2c320b0f88bb0457a9d11b8f1aa10419b35
Nader <ns1568@fayoum.edu.eg>
2023-11-14 01:48:42 +0200

    convert to stateless widget

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -2,14 +2,9 @@ import 'package:flutter/material.dart';
 import 'widgets/home widgets/custom_home_appbar.dart';
 import 'widgets/home widgets/home_view_body.dart';
 
-class HomeView extends StatefulWidget {
+class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
-  @override
-  State<HomeView> createState() => _HomeViewState();
-}
-
-class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
