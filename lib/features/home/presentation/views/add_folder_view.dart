51d0571b64926299bcedd5c77c54088398fecb12
Nader <ns1568@fayoum.edu.eg>
2023-11-14 02:05:01 +0200

    add button to push notification

--- lib/features/home/presentation/views/add_folder_view.dart
+++ lib/features/home/presentation/views/add_folder_view.dart
@@ -1,9 +1,22 @@
+import 'package:firebase_training/core/widgets/custom_toast.dart';
+import 'package:firebase_training/features/home/presentation/manager/message%20cubit/message_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'widgets/add folder/add_folder_view_body.dart';
 
-class AddFolderView extends StatelessWidget {
+class AddFolderView extends StatefulWidget {
   const AddFolderView({super.key});
+
+  @override
+  State<AddFolderView> createState() => _AddFolderViewState();
+}
+
+class _AddFolderViewState extends State<AddFolderView> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +25,17 @@ class AddFolderView extends StatelessWidget {
         title: const Text('Add folder'),
         centerTitle: true,
       ),
-      body: const AddFolderViewBody(),
+      body: BlocListener<MessageCubit, MessageState>(
+        listener: (context, state) {
+          if (state is MessageSuccess) {
+            customToast(message: 'success');
+          } else if (state is MessageFailure) {
+            customToast(
+                backgroundColor: Colors.red, message: state.errorMessage);
+          }
+        },
+        child: const AddFolderViewBody(),
+      ),
     );
   }
 }
